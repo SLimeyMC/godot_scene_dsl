5eb8d80353c48a86f172a15ace3b2449292487c4
SlimeyAr <lostbhijaya@gmail.com>
2024-08-03 19:02:14 +0700

    initialize some stuff idk

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("com.utopia-rise.godot-kotlin-jvm") version "0.9.1-4.2.2"
+}
+
+group = "io.github.slimeymc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
