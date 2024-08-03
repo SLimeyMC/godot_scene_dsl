0f4e8d03b4b5b1189b736bf742ac8935984e9667
SlimeyAr <lostbhijaya@gmail.com>
2024-08-03 16:34:27 +0700

    Change function name to be proper

--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -6,17 +6,17 @@ import godot.Script
 
 
 inline fun Node(vararg attributes: Pair<String, Any> = emptyArray(), script: String? = null, noinline children: () -> List<Node> = { emptyList() })
-        = CreateNode(Node::class.java, emptyList(), *attributes, script = script, children = children)
+        = createNode(Node::class.java, emptyList(), *attributes, script = script, children = children)
 
 
-inline fun ProcessMode(mode: Node.ProcessMode) = "process_mode" to mode
+inline fun processMode(mode: Node.ProcessMode) = "process_mode" to mode
 
-inline fun ProcessPriority(priority: Int) = "process_priority" to priority
+inline fun processPriority(priority: Int) = "process_priority" to priority
 
-inline fun ProcessPhysicsPriority(priority: Int) = "process_physics_mode" to priority
-
-
-inline fun ProcessThreadGroup(group: Node.ProcessThreadGroup) = "process_thread_group" to group
+inline fun processPhysicsPriority(priority: Int) = "process_physics_mode" to priority
 
 
-inline fun Script(script: Script) = "script" to script
+inline fun processThreadGroup(group: Node.ProcessThreadGroup) = "process_thread_group" to group
+
+
+inline fun script(script: Script) = "script" to script
